154d2c0f5c320a882ae4b2f2e632990756069af7
Gina <9094115+ginazampino@users.noreply.github.com>
2023-10-20 09:13:27 -0400

    Work toward setup

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,5 +1,4 @@
 import '@/styles/reset.css';
-import '@/styles/globals.css';
 
 import type { Metadata } from 'next';
 
@@ -11,8 +10,8 @@ const sourceSans3 = Source_Sans_3({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
     title: 'Gina Zampino | Design & Development',
-    description:
-        'My handmade websites are tailored for entrepreneurs and small businesses. All work produced locally in Raleigh-Durham, NC.',
+    description: `My handmade websites are tailored for entrepreneurs and small businesses. 
+        All work produced locally in Raleigh-Durham, NC.`,
 };
 
 export default function RootLayout({
